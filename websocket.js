25f3c2a294acfa3b3f1306450f6ddf756f2fd86e
Jose Barros <jbarros35@users.noreply.github.com>
2018-04-11 14:31:43 +0100

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -4,54 +4,56 @@ var https = require('https');
 var fs = require('fs');
 const WebSocket = require('ws');
 Tail = require('tail').Tail;
-
+// certificate files
 var privateKey  = fs.readFileSync('./key.pem', 'utf8');
 var certificate = fs.readFileSync('./cert.pem', 'utf8');
+var certpass = process.env.ENV_CERTPASS;
 
+// certificate credentials
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
-var certpass = process.env.ENV_CERTPASS;
-
+// options for load certificate
 var options = {
   key: privateKey,
   cert: certificate,
   passphrase: certpass
 };
-
+// create https server
 var server = https.createServer(options, function (req, res) {
   res.writeHead(200);
   res.end("hello world\n");
 });
-//.listen(443);
 
+// create secure websocket
 const wss = new WebSocket.Server({ server });
-
+// connect socket
 wss.on('connection', function connection(ws, req) {
-  
+  // get client ip
   const ip = req.connection.remoteAddress;
   console.log('client ip'+ip);
 
-ws.on('message', function incoming(message) {
+   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
-
+  // ping client
   ws.send('something your ip '+ip);
 });
-
+// start server at 443
 server.listen(443);
 
-tail = new Tail("/home/ubuntu/geth/nohup.out");
+// tail a geth output file
+var logfile = process.env.ENV_GETHLOG;
+tail = new Tail(logfile);
 
 tail.on("line", function(data) {
   
-wss.clients.forEach(function each(ws) {
-  console.log(data);
-  if (ws.isAlive === false) return ws.terminate();
-    ws.isAlive = false;
-    ws.ping(data);
-  });
-
+  wss.clients.forEach(function each(ws) {
+    console.log(data);
+    if (ws.isAlive === false) return ws.terminate();
+      ws.isAlive = false;
+      ws.ping(data);
+    });
 });
 
 tail.on("error", function(error) {
